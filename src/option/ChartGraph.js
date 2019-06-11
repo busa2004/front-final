875d972df5cc3b3734a3bff68feb7298acc31695
nam <wadzj3@gmail.com>
2019-06-11 09:40:39 +0900

    a

---
+++ src/option/ChartGraph.js
@@ -0,0 +1,43 @@
+import { Doughnut } from 'react-chartjs-2';
+import React, { Component } from 'react';
+class ChartGraph extends Component {
+    constructor(props) {
+        super(props);
+        this.state ={
+            data:{
+                datasets: [{
+                    data: [this.props.data[0].count,this.props.data[1].count],
+                    backgroundColor: [ 
+                    'rgba(255,255,192,1)',
+                    'rgba(255,192,192,1)']
+                }],
+            
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: [
+                    this.props.data[0].status,this.props.data[1].status
+                   
+                ],
+               
+            },
+            
+           }
+       
+            
+    }
+    render () {
+        return (
+       <Doughnut
+        data={this.state.data}
+        options={{
+          responsive: true,
+          maintainAspectRatio: true,
+        }}
+      />
+ 
+            
+            )
+          
+    }
+}
+
+export default ChartGraph;
--- src/option/Graph.js
+++ src/option/Graph.js
@@ -2,76 +2,88 @@ import { Doughnut } from 'react-chartjs-2';
 import React, { Component } from 'react';
 import { Card } from 'antd';
 import { getGraph } from '../util/APIUtils';
-
+import LoadingIndicator from '../common/LoadingIndicator';
+import ChartGraph from './ChartGraph'
+import ServerError from '../common/ServerError';
+import NotFound from '../common/NotFound';
 class Graph extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            data:{
-                datasets: [{
-                    data: [ this.state.report[0].count, this.state.report[1].count],
-                    backgroundColor: [ 
-                    'rgba(255,255,192,1)',
-                    'rgba(255,192,192,1)']
-                }],
-            
-                // These labels appear in the legend and in the tooltips when hovering different arcs
-                labels: [
-                   
-                    this.state.report[0].status,
-                    this.state.report[1].status
-                ],
-               
-            },
-            
-        }
-       
+      
+       this.state={
+           data:null
+       }
             
     }
-
     loadGraph() {
         this.setState({
             isLoading: true
         });
-      
         getGraph()
-        .then(response => {
-            this.setState({
-                report: response,
-                isLoading: false
-            });
-        console.log(this.state.report)
-        }).catch(error => {
-            if(error.status === 404) {
+            .then(response => {
+               
                 this.setState({
-                    notFound: true,
+                    data: response,
                     isLoading: false
                 });
-            } else {
-                this.setState({
-                    serverError: true,
-                    isLoading: false
-                });        
+                console.log(this.state.data)
+            }).catch(error => {
+                if (error.status === 404) {
+                    this.setState({
+                        notFound: true,
+                        isLoading: false
+                    });
+                } else {
+                    this.setState({
+                        serverError: true,
+                        isLoading: false
+                    });
+                }
+            });
+    }
+    branch(){
+      
+        
+        if(Object.keys(this.state.data).length === 0 ){
+           <p>결제된 보고서가 존재하지 않습니다.</p>
+        }
+        else if(Object.keys(this.state.data).length === 1 ){
+            
+            if(this.state.data[0].status ==='HOLD')
+            this.state.data.push({count:0,status:'PROGRESS'})
+            else{
+            this.state.data.push({count:0,status:'HOLD'})
             }
-        });        
-      }
+            return <ChartGraph data={this.state.data}/>
+        }else if(Object.keys(this.state.data).length ===2 ){
+
+        return <ChartGraph data={this.state.data}/>
+        }
+    }
     componentWillMount(){
         this.loadGraph()
     }
     render () {
+    if(this.state.isLoading) {
+            return <LoadingIndicator />
+    }
+    if(this.state.notFound) {
+        return <NotFound />;
+      }
+      
+      if(this.state.serverError) {
+        return <ServerError />;
+      }
         return (
+            <div>
             <Card headStyle={{backgroundColor:"#00B1B6",color:"#FBFBFB",fontWeight:"bold"}}
             bodyStyle={{backgroundColor:"16448250"}}  title='업무리스트'>
-       <Doughnut
-        data={this.state.data}
-        options={{
-          responsive: true,
-          maintainAspectRatio: true,
-        }}
-      />
-            </Card>    
-            
+       {this.branch()}   
+        </Card> 
+        
+        </div>
             )
+          
     }
 }
 
